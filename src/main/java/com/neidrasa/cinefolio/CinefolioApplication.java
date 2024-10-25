09e2b27bd404393d0c452d4848ba53fb97c324af
neidrasa <adrien.salez@outlook.fr>
2024-10-25 17:18:29 +0200

    0%

---
+++ src/main/java/com/neidrasa/cinefolio/CinefolioApplication.java
@@ -0,0 +1,13 @@
+package com.neidrasa.cinefolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinefolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neidrasa/cinefolio/CinefolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neidrasa.cinefolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinefolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
